4503c212cfbdc9a77ff33fb7ffe438c2409c2df5
sanketpatil461 <140166266+sanketpatil461@users.noreply.github.com>
2025-01-11 12:27:25 +0530

    myaddition.txt

---
+++ addition.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 13 18:23:02 2023
+
+@author: sanke
+"""
+
+a=int(input("enter first number:"))
+b=int(input("enter secuond number:"))
+c=a+b
+print(c)
